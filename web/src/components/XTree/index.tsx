e2fd278779c8a48303194b1b7a97fdf754d5f0f5
Zolo <zolo@onezol.com>
2023-06-24 12:24:58 +0800

    fix: XTree样式优化

--- web/src/components/XTree/index.tsx
+++ web/src/components/XTree/index.tsx
@@ -1,35 +1,38 @@
-import {Tree} from "antd";
+import {Card, Tree} from "antd";
 import {useEffect, useState} from "react";
 import {defaultTreeData, treeDataHandler} from "@/utils/treeify";
 import {AntTree} from "@/typings";
+import styles from './style.less';
 
 interface Props {
-    rootTitle: string;
-    treeNodes: AntTree.TreeNode[];
-    onSelect?: AntTree.onSelected;
+  rootTitle: string;
+  treeNodes: AntTree.TreeNode[];
+  onSelect?: AntTree.onSelected;
+  style?: React.CSSProperties;
 }
 
 const XTree: React.FC<Props> = (props) => {
-    const {rootTitle, treeNodes, onSelect} = props;
-    const [treeData, setTreeData] = useState<AntTree.TreeNode[]>(defaultTreeData(rootTitle));
+  const {rootTitle, treeNodes, onSelect, style} = props;
+  const [treeData, setTreeData] = useState<AntTree.TreeNode[]>(defaultTreeData(rootTitle));
 
-    useEffect(() => {
-        const treeData = treeDataHandler(treeNodes, rootTitle);
-        setTreeData([...treeData]);
-    }, [treeNodes]);
+  useEffect(() => {
+    const treeData = treeDataHandler(treeNodes, rootTitle);
+    setTreeData([...treeData]);
+  }, [treeNodes]);
 
-    const doSelect: AntTree.onSelected = (selectedKeys, info) => {
-        onSelect?.(selectedKeys, info);
-    };
+  const doSelect: AntTree.onSelected = (selectedKeys, info) => {
+    onSelect?.(selectedKeys, info);
+  };
 
-    return (
-        <Tree
-            style={{margin: 20}}
-            defaultExpandedKeys={['root']}
-            onSelect={doSelect}
-            treeData={treeData}
-        />
-    )
+  return (
+    <Card className={styles.wrapper} style={style} bordered={false}>
+      <Tree
+        defaultExpandedKeys={['root']}
+        onSelect={doSelect}
+        treeData={treeData}
+      />
+    </Card>
+  )
 
 }
 
